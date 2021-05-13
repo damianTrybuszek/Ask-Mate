260cdb04c82dc95b21c6b04c4d6244d9a2721f96
Damian Trybuszek <damian.trybuszek@gmail.com>
2021-05-13 17:55:19 +0200

    My own bug deleted.

--- server.py
+++ server.py
@@ -14,10 +14,5 @@ def list():
     headers = data_handling.get_headers()
     return render_template("list.html", question_list=question_list, headers=headers)
 
-@app.route("/question/question_id")
-def display():
-    question_list = data_handling.get_questions()
-    question_id = 
-
 if __name__ == "__main__":
     app.run(debug=True)
